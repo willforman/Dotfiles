d75e63ca305375f7b2077f2fcb4de6299ba8051e
willforman <wf8581@gmail.com>
2022-12-03 14:12:38 -0500

    Update formatting

--- .config/nvim/lua/formatter.lua
+++ .config/nvim/lua/formatter.lua
@@ -1,17 +1,34 @@
 local null_ls = require('null-ls')
 
+-- Make sure only use null-ls for formatting
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format({
+    filter = function(client)
+      return client.name == "null-ls"
+    end,
+    bufnr = bufnr
+  })
+end
+
+-- Format on save
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local on_attach = function(client, bufnr)
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        lsp_formatting(bufnr)
+      end,
+    })
+  end
+end
+
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettierd,
   },
-  on_attach = function(client)
-        if client.resolved_capabilities.document_formatting then
-            vim.cmd([[
-            augroup LspFormatting
-                autocmd! * <buffer>
-                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-            augroup END
-            ]])
-        end
-    end,
+  on_attach = on_attach,
 })
