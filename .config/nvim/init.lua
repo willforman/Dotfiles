1d55dca4f007af5600c847eb62348aa7d25fa8b9
willforman <wf8581@gmail.com>
2022-12-03 13:46:24 -0500

    Separate vim settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,4 @@
-vim.g.mapleader = " "
-
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
-vim.opt.expandtab = true
-vim.opt.autoindent = true
-vim.opt.number = true
-vim.opt.autoindent = true
-
+require('vim-settings')
 require('plugins')
 require('lsp')
 require('treesitter')
